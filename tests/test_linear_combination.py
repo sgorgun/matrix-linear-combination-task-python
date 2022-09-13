f60817c2ecce056f9d0be35685a850249c85d663
Dzmitry Dzemidka <ddzemidka@estee.com>
2022-09-13 21:00:39 +0300

    fixes

--- tests/test_linear_combination.py
+++ tests/test_linear_combination.py
@@ -10,10 +10,11 @@ def test_linear_combination():
     """Sample tests for linear_combination function."""
     assert np.allclose(linear_combination(matrix=[[1, 0], [0, 1]], weights=[2, 3]), [2, 3])
     
-    assert np.allclose(linear_combination(matrix=[[1, 1, 1], [1, 2, 3]], weights=[1, 2]), [3, 5, 7])
+    assert np.allclose(linear_combination(matrix=[[1, 1, 1], [1, 2, 3]], weights=[1, 2, 1]), [4, 8])
 
     with pytest.raises(ValueError):
         _ = linear_combination(matrix=[[1, 1], [1, 1]], weights=[2])
 
     with pytest.raises(ValueError):
         _ = linear_combination(matrix=[[1, 1], [1, 1]], weights=[2, 3, 4])
+    
